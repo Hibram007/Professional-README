fed7e2050e2fd2019828b409f9793d13c499a586
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-26 17:50:54 -0700

    all prompt questions added, display of input needed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 
-// wrpaing function to to return inquirer returned data
+// wrapping function to to return inquirer returned data
 const promptUser = () => {
     return inquirer.prompt([
         //project questions
@@ -14,7 +14,49 @@ const promptUser = () => {
             type: 'input',
             name: 'Description',
             message: 'Enter a description of your project',
-          }
+        },
+         {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide instructions for installation',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Provide any usage information',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide instructions for installation',
+        },
+        {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What License did you use for this project? (Check all that apply)',
+        choices: ['Artistic license 2.0', 'Do What The F*ck You Want To Public License', 'MIT', 'Open Software License 3.0']
+        },
+        {
+            type: 'input',
+            name: 'Contributions',
+            message: 'Provide any guidelines for contributing to the project',
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'Provide any test instructions',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Provide your Github username',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your email address for questions regarding the project',
+        },
+
     ]);
 };
 
